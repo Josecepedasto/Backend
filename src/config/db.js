c3cefb6f971a44f48a73f77a675cdb65a0503939
Josecepedasto <138180044+Josecepedasto@users.noreply.github.com>
2024-04-17 08:56:15 -0600

    Update3 db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = "mongodb+srv://pepecep113:im_#U3grg4fHG!B@cluster0.weo6nu4.mongodb.net/";
+const MONGO_URL = "mongodb+srv://pepecep13:pepecep13@cluster0.rrjb1rd.mongodb.net/";
 
 export const connectDB = async () => {
   try {
