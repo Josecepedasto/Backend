f1fca6712e16ba93fd7cdc601ef1226c670fe886
Josecepedasto <138180044+Josecepedasto@users.noreply.github.com>
2024-04-17 08:23:44 -0600

    Add files via upload

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const MONGO_URL = "mongodb+srv://pepecep13:kegopez01@cluster0.rrjb1rd.mongodb.net/";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("connect mongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
